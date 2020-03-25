e4f3b226618f7c4bfa3a9eacc1ff890d8dd3401f
Timinii <61969494+Timinii@users.noreply.github.com>
2020-03-25 21:44:51 +0100

    Create Radius.py

---
+++ Radius.py
@@ -0,0 +1,5 @@
+Pi = 3.14
+radius = float(input(' Welcome, please input the radius of your circle: '))
+area = Pi * (radius **2)
+print(" The Area Of Your Circle = %.2f\n" %area)
+print('Area of a circle task complete!')
